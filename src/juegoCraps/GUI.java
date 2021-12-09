94954718e87d917e5bd1a2093b93e3d22396ea6a
Javier-C0L <89871899+Javier-C0L@users.noreply.github.com>
2021-12-09 18:30:30 -0500

    Add files via upload

---
+++ src/juegoCraps/GUI.java
@@ -0,0 +1,135 @@
+package juegoCraps;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * This class is used as View Craps Class
+ * @author Javier Castrillon 1827486 - 2711 javier.castrillon@correounivalle.edu.co
+ * @version v.1.0.0 date:21/11/2021
+ */
+public class GUI extends JFrame {
+    private static final String MENSAJE_INICIO="Bienvenido a Craps "
+            + "\nOprime el botón lanzar para iniciar el juego"
+            + "\nSi tu tiro de salida es 7 u 11 ganas con Natural"
+            + "\nSi tu tiro de salida es 2, 3 o 12 pierdes con Craps"
+            + "\nSi sacas cualquier otro valor establecerás el Punto"
+            + "\nEstado en punto podrás seguir lanzando los dados"
+            + "\npero ahora ganarás si sacas nuevamente el valor del Punto "
+            + "\nsin que previamente hayas sacado 7";
+
+    private Header headerProject;
+    private JLabel dado1, dado2;
+    private JButton lanzar;
+    private JPanel panelDados, panelResultados;
+    private ImageIcon imageDado;
+    private JTextArea mensajesSalida,resultadosDados;
+    private JSeparator separator;
+    private Escucha escucha;
+    private ModelCraps modelCraps;
+    /**
+     * Constructor of GUI class
+     */
+    public GUI(){
+        initGUI();
+
+        //Default JFrame configuration
+        this.setTitle("Juego Craps");
+        //this.setSize(200,100);
+        this.pack();
+        this.setResizable(true);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /**
+     * This method is used to set up the default JComponent Configuration,
+     * create Listener and control Objects used for the GUI class
+     */
+    private void initGUI() {
+        //Set up JFrame Container's Layout
+        //Create Listener Object or Control Object
+        escucha = new Escucha();
+        modelCraps = new ModelCraps();
+
+        //Set up JComponents
+        headerProject = new Header("Mesa Juego Craps", Color.BLACK);
+        this.add(headerProject,BorderLayout.NORTH);
+
+        imageDado = new ImageIcon(getClass().getResource("/resources/dado.png"));
+        dado1 = new JLabel(imageDado);
+        dado2 = new JLabel(imageDado);
+
+        lanzar = new JButton("lanzar");
+        lanzar.addActionListener(escucha);
+
+        panelDados = new JPanel();
+        panelDados.setPreferredSize(new Dimension(300, 180));
+        panelDados.setBorder(BorderFactory.createTitledBorder("Tus Dados "));
+        panelDados.add(dado1);
+        panelDados.add(dado2);
+        panelDados.add(lanzar);
+
+        this.add(panelDados,BorderLayout.CENTER);
+
+        mensajesSalida = new JTextArea(7,31);
+        mensajesSalida.setText(MENSAJE_INICIO);
+        //mensajesSalida.setBorder(BorderFactory.createTitledBorder("Que debes hacer "));
+        JScrollPane scroll = new JScrollPane(mensajesSalida);
+
+        panelResultados = new JPanel();
+        panelResultados.setBorder(BorderFactory.createTitledBorder("Que debes hacer "));
+        panelResultados.add(scroll);
+        panelResultados.setPreferredSize(new Dimension(370,180));
+
+        this.add(panelResultados,BorderLayout.EAST);
+
+        resultadosDados = new JTextArea(4, 31);
+        separator = new JSeparator();
+        separator.setPreferredSize(new Dimension(320,7));
+        separator.setBackground(Color.BLUE);
+    }
+
+    /**
+     * Main process of the Java program
+     * @param args Object used in order to send input data from command line when
+     *             the program is execute by console.
+     */
+    public static void main(String[] args){
+        EventQueue.invokeLater(() -> {
+            GUI miProjectGUI = new GUI();
+        });
+    }
+
+    /**
+     * inner class that extends an Adapter Class or implements Listeners used by GUI class
+     */
+    private class Escucha implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            modelCraps.calcularTiro();
+            int [] caras = modelCraps.getCaras();
+            imageDado = new ImageIcon(getClass().getResource("/resources/"+caras[0]+".png"));
+            dado1.setIcon(imageDado);
+            imageDado = new ImageIcon(getClass().getResource("/resources/"+caras[1]+".png"));
+            dado2.setIcon(imageDado);
+            modelCraps.determinarJuego();
+            ////////////    posible optimisación ////////////
+            panelResultados.removeAll();
+            panelResultados.setBorder(BorderFactory.createTitledBorder("Resultados"));
+            panelResultados.add(resultadosDados);
+            panelResultados.add(separator);
+            panelResultados.add(mensajesSalida);////////////
+            resultadosDados.setText(modelCraps.getEstadoToString()[0]);
+            mensajesSalida.setRows(4);
+            mensajesSalida.setText(modelCraps.getEstadoToString()[1]);
+            revalidate();
+            repaint();
+            //resultadosDados.setText(modelCraps.getEstadoToString());
+        }
+    }
+}
---
+++ src/juegoCraps/ModelCraps.java
@@ -0,0 +1,123 @@
+package juegoCraps;
+
+/**
+ * ModelCraps apply craps rules.
+ * estado = 1 natural winner
+ * estado = 2 Craps looser
+ * estado = 3 Establish Punto
+ * estado = 4 Punto winner
+ * estado = 5 Punto loser
+ * @author Javier Castrillon
+ * @version V.1.0.0 date 06/12/2021
+ */
+public class ModelCraps {
+    private Dado dado1, dado2;
+    private int tiro, punto, estado, flag;
+    private String[] estadoToString;
+    private int[] caras;
+
+    /**
+     * Class Constructor
+     */
+    public ModelCraps(){
+        dado1 = new Dado();
+        dado2 = new Dado();
+        caras = new int[2];
+        estadoToString = new String[2];
+        flag = 0;
+    }
+
+    /**
+     * Establish the tiro value according to each dice
+     */
+    public void calcularTiro(){
+        caras[0]=dado1.getCara();
+        caras[1]=dado2.getCara();
+        tiro = caras[0] + caras[1];
+    }
+
+    /**
+     * Establish game state according to estado attribute value
+     * estado = 1 natural winner
+     * estado = 2 Craps looser
+     * estado = 3 Establish Punto
+     */
+    public void determinarJuego(){
+        if(flag==0){
+            if(tiro==7 || tiro==11){
+                estado = 1;
+            }else if(tiro==3 || tiro==2){
+                estado = 2;
+            }else{
+                estado = 3;
+                punto=tiro;
+                flag=1;
+            }
+        }else{
+            //ronda punto
+            rondaPunto();
+        }
+
+    }
+
+    /**
+     * Establish game state according to estado attribute value
+     * estado = 4 Punto winner
+     * estado = 5 Punto loser
+     */
+    private void rondaPunto() {
+        if(tiro==punto){
+            estado=4;
+            flag=0;
+        }else if(tiro==7){
+            estado=5;
+            flag=0;
+        }else{  estado=6;   }
+    }
+
+    public int getTiro() {
+        return tiro;
+    }
+
+    public int getPunto() {
+        return punto;
+    }
+
+    /**
+     * Establish message game state according to estado attribute value
+     * @return Message for the View class
+     */
+    public String[] getEstadoToString() {
+        switch(estado){
+            case 1: estadoToString[0]="Tiro de Salida = "+tiro;
+                    estadoToString[1]= "Sacaste Natural, has ganado!!";
+                    break;
+            case 2: estadoToString[0]="Tiro de Salida = "+tiro;
+                    estadoToString[1]= "Sacaste Craps, has perdido!!";
+                    break;
+            case 3: estadoToString[0]="Tiro de Salida = "+tiro+"\nPunto = "+punto;
+                    estadoToString[1]= "Estableciste punto en "+punto+
+                                    " debes seguir lanzando!!"+
+                                    "\npero si sacas 7 antes que "+punto+" perderás";
+                    break;
+            case 4: estadoToString[0]="Tiro de Salida = "+punto+"\nPunto = "+punto
+                                     +"\nValor del nuevo Tiro = "+tiro;
+                    estadoToString[1]= "Volviste a sacar "+punto+", has ganado!!";
+                    break;
+            case 5: estadoToString[0]="Tiro de Salida = "+punto+"\nPunto = "+punto
+                    +"\nValor del nuevo Tiro = "+tiro;
+                    estadoToString[1]= "Sacaste 7 antes que "+punto+", has perdido!!";
+                    break;
+            case 6: estadoToString[0]="Tiro de Salida = "+punto+"\nPunto = "+punto
+                    +"\nValor del nuevo Tiro = "+tiro;
+                    estadoToString[1]="Estas en Punto y debes seguir lanzando!!"
+                                     +"\npero si sacas 7 antes que "+punto+" perderás";
+                    break;
+        }
+        return estadoToString;
+    }
+
+    public int[] getCaras() { return caras; }
+
+    public int getFlag() {return flag;}
+}
